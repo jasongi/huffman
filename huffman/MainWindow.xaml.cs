55fe88544421417a74a8ed1a3a546c70624f1b9c
jasongi <jasongiancono@gmail.com>
2015-07-20 22:59:07 +0800

    copyrights

--- huffman/MainWindow.xaml.cs
+++ huffman/MainWindow.xaml.cs
@@ -14,6 +14,19 @@ using System.Windows.Shapes;
 using System.Collections;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
+/*  
+    Copyright (C) 2014 Jason Giancono (jasongiancono@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
 namespace Asgn
 {
     /// <summary>
